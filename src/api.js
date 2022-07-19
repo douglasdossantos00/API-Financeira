24d094181126a5179b3977ea89f2250f4cc9fdcb
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-19 15:49:34 -0300

    feat: criando middleware de erro

--- src/api.js
+++ src/api.js
@@ -1,10 +1,24 @@
 require('express-async-errors');
 const express = require('express');
+const AppError = require('./middleware/Error');
 
 const app = express();
-const router = ('./routers');
+const router = require('./routers');
 
 app.use(express.json());
 app.use(router);
+
+app.use((err, _request, response, next) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ message: err.message });
+  }
+  if (!err.statusCode) {
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+  return next();
+});
 
 module.exports = app;
