26e3369ff97ec1137730d54f611266bdb6fb083a
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-21 13:03:07 -0300

    feat: criando jwt

---
+++ src/utils/jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('../middleware/Error');
+
+const TOKEN_SECRET = process.env.JWT_SECRET;
+
+const jwtConfig = {
+  expiresIn: '240h',
+  algorithm: 'HS256',
+};
+
+const generateJWTToken = (user) => jwt.sign({ user }, TOKEN_SECRET, jwtConfig);
+
+const authenticateToken = async (token) => {
+  if (!token) {
+    throw new AppError('Token não existe', 401);
+  }
+
+  try {
+    const validate = jwt.verify(token, TOKEN_SECRET);
+    return validate;
+  } catch (error) {
+    console.log(error);
+    throw new AppError('Token expirado ou não é válido', 401);
+  }
+};
+
+module.exports = {
+  generateJWTToken,
+  authenticateToken,
+};
