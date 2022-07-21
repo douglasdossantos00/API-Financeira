ea2db91180e8414fec6539cc02f7f37e6068532b
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-21 19:19:44 -0300

    feat: criando as rota para os endpoints Ativo

--- src/routers/ativosRouter.js
+++ src/routers/ativosRouter.js
@@ -5,7 +5,7 @@ const ativoRouter = Router();
 const ativoController = require('../controllers/ativoController');
 
 ativoRouter.get('/', ativoController.getAllAtivosController);
-ativoRouter.get('/:codCliente');
-ativoRouter.get('/:codAtivo');
+ativoRouter.get('/user/:codCliente', ativoController.getAllAtivosUserController);
+ativoRouter.get('/:codAtivo', ativoController.getAtivoByIdService);
 
 module.exports = ativoRouter;
