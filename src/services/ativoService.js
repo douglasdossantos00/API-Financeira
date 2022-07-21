c083b317272b1d59dd5291fe870d0f55d99c7543
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-21 19:20:19 -0300

    feat: criando as services para os endpoints Ativo

--- src/services/ativoService.js
+++ src/services/ativoService.js
@@ -1,9 +1,26 @@
-const { Ativo } = require('../database/models');
+const { Ativo, sequelize } = require('../database/models');
 
 const getAllAtivosService = async () => {
   const allAtivos = await Ativo.findAll({
-    attributes: { exclude: ['id '] },
+    attributes: { exclude: ['createdAt', 'updatedAt'] },
   });
   return allAtivos;
 };
-module.exports = { getAllAtivosService };
+
+const getAllAtivosUserService = async (codCliente) => {
+  const allAtivosUser = await sequelize.query(`SELECT C.idUser, A.id,
+  A.codAtivo, AC.qtdeAtivo, A.valor FROM desafio_xp_dev.AtivosContas as AC
+  join desafio_xp_dev.Ativos as A on AC.idAtivo = A.id
+  join desafio_xp_dev.Contas as C on AC.idConta = C.idUser
+  where C.idUser = ${codCliente}`);
+  return allAtivosUser[0];
+};
+
+const getAtivoByIdService = async (codAtivo) => {
+  const findAtivo = await Ativo.findOne({
+    where: { id: codAtivo },
+    attributes: { exclude: ['createdAt', 'updatedAt'] },
+  });
+  return findAtivo;
+};
+module.exports = { getAllAtivosService, getAllAtivosUserService, getAtivoByIdService };
