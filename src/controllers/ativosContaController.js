6d2313b5d36b958c42c0d40c5d503ec460c80dba
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-22 15:31:17 -0300

    fix: excluindo arquivo AtivosContas

--- src/controllers/ativosContaController.js
+++
@@ -1,9 +0,0 @@
-const ativoContaService = require('../services/ativosContasService');
-
-const getAllAtivosContasController = async (request, response) => {
-  console.log(request.body);
-  const allAtivosContas = await ativoContaService.getAllAtivosContasService();
-  response.status(200).json(allAtivosContas);
-};
-
-module.exports = { getAllAtivosContasController };
--- src/services/ativosContasService.js
+++
@@ -1,9 +0,0 @@
-const { AtivoConta } = require('../database/models');
-
-const getAllAtivosContasService = async () => {
-  const allAtivos = await AtivoConta.findAll({
-    attributes: { exclude: ['id '] },
-  });
-  return allAtivos;
-};
-module.exports = { getAllAtivosContasService };
