c9e4a66542b990b4891fde0b7a8fc617404eba91
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-22 15:28:47 -0300

    fix: mudando o nome da rota

--- src/routers/ativosRouter.js
+++ src/routers/ativosRouter.js
@@ -5,7 +5,7 @@ const ativoRouter = Router();
 const ativoController = require('../controllers/ativoController');
 
 ativoRouter.get('/', ativoController.getAllAtivosController);
-ativoRouter.get('/user/:codCliente', ativoController.getAllAtivosUserController);
+ativoRouter.get('/users/:codCliente', ativoController.getAllAtivosUserController);
 ativoRouter.get('/:codAtivo', ativoController.getAtivoByIdService);
 
 module.exports = ativoRouter;
