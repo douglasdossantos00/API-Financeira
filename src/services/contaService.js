5ff6896fcd8fb6831ddce16cd45badf47a21efc0
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-22 15:48:57 -0300

    style: apgando console log

--- src/services/contaService.js
+++ src/services/contaService.js
@@ -4,7 +4,7 @@ const AppError = require('../middleware/Error');
 const getContaService = async (codCliente) => {
   const getCliente = await Conta.findOne({ where: { idUser: codCliente } });
   if (!getCliente) {
-    throw new AppError('Conta não encontrada', 404);
+    throw new AppError('Conta não encontrada');
   }
   return getCliente;
 };
@@ -12,7 +12,7 @@ const getContaService = async (codCliente) => {
 const incrementContaService = async (codCliente, valor) => {
   const { idUser } = await getContaService(codCliente);
   if (valor <= 0) {
-    throw new AppError('Solicitação inválida');
+    throw new AppError('Quantidade a ser depositada não pode ser menor ou igual a zero');
   }
   const findConta = await Conta.increment({ saldo: valor }, { where: { idUser } });
   return findConta;
@@ -21,7 +21,7 @@ const incrementContaService = async (codCliente, valor) => {
 const decrementContaService = async (codCliente, valor) => {
   const { idUser, saldo } = await getContaService(codCliente);
   if (valor <= 0 || valor > saldo) {
-    throw new AppError('Solicitação inválida');
+    throw new AppError('Quantidade a ser sacada não pode ser maior que o saldo da conta e não pode ser menor ou igual a zero');
   }
   const findConta = await Conta.increment({ saldo: -valor }, { where: { idUser } });
   return findConta;
