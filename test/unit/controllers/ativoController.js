11dfe85760fe0a35f64cc633d0b549de43ef4238
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-07-24 22:12:22 -0300

    feat: testes do ativo controller

--- test/unit/controllers/ativoController.js
+++ test/unit/controllers/ativoController.js
@@ -5,22 +5,21 @@ const ativoService = require('../../../src/services/ativoService');
 
 // teste get allAllAtivoService
 
-const allAtivos = [
-  {
-    id: 1,
-    codAtivo: 'ALPA4',
-    qtdeAtivo: 99124,
-    valor: '19.59',
-  },
-  {
-    id: 2,
-    codAtivo: 'MRFG3',
-    qtdeAtivo: 300000,
-    valor: '12.65',
-  },
-];
-
-describe('Consulta todos os produtos com retorno da função getAllAtivo no Controllers', async () => {
+describe('Consulta todos os ativos com retorno da função getAllAtivo no Controllers', async () => {
+  const allAtivos = [
+    {
+      id: 1,
+      codAtivo: 'ALPA4',
+      qtdeAtivo: 99124,
+      valor: '19.59',
+    },
+    {
+      id: 2,
+      codAtivo: 'MRFG3',
+      qtdeAtivo: 300000,
+      valor: '12.65',
+    },
+  ];
   const request = {};
   const response = {};
 
@@ -34,9 +33,32 @@ describe('Consulta todos os produtos com retorno da função getAllAtivo no Cont
     ativoService.getAllAtivosService.restore();
   });
   it('retorna um array', async () => {
-    const products = await ativoController.getAllAtivosController(request, response);
-    console.log('teste', products);
-    expect(products).to.be.a('array');
+    await ativoController.getAllAtivosController(request, response);
+    expect(response.json.calledWith(allAtivos)).to.be.equal(true);
+    expect(response.status.calledWith(200)).to.be.equal(true);
+  });
+});
+
+describe('Consulta os ativos por id ', async () => {
+  const request = { params: { codAtivo: 1 } };
+  const response = {};
+  before(() => {
+    const ativo = {
+      id: 1,
+      codAtivo: 'ALPA4',
+      qtdeAtivo: 99114,
+      valor: '19.59',
+    };
+    sinon.stub(ativoService, 'getAtivoByIdService').resolves(ativo);
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns(ativo);
+  });
+
+  after(() => {
+    ativoService.getAtivoByIdService.restore();
+  });
+  it('retorna um objeto', async () => {
+    await ativoController.getAtivoByIdController(request, response);
     expect(response.status.calledWith(200)).to.be.equal(true);
   });
 });
